292da8e069500b297b7eb601b81be93e2c2b55d3
Mihir Khare <mkhare429@gmail.com>
2020-10-05 01:23:16 +0530

    make it better

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const signUp = require('./signup');
+const signIn = require('./signin');
 
 router.post('/signup',signUp);
+router.post('/signin',signIn);
 
 module.exports = router
