e60f8de782f8b2e3ade48b7496024b65e750d8f0
Mihir Khare <mkhare429@gmail.com>
2020-10-12 23:29:26 +0530

    Cors Added

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ require('dotenv').config()
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3005;
+const cors = require('cors');
 const userRoutes = require('./routes/index');
 require('./db/connection');
 
+app.use(cors())
 app.use('/uploads',express.static(__dirname+'uploads'));
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
