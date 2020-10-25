056a1fcc7cb258c205787f63a1d70742b84b2ecc
Mihir Khare <mkhare429@gmail.com>
2020-10-25 18:14:23 +0530

    Parametre Support

--- server.js
+++ server.js
@@ -4,12 +4,13 @@ const app = express();
 const port = process.env.PORT || 3005;
 const cors = require('cors');
 const userRoutes = require('./routes/index');
+const bodyParser = require('body-parser')
 require('./db/connection');
 
 app.use(cors())
 app.use('/uploads',express.static(__dirname+'uploads'));
-app.use(express.urlencoded({extended:true,limit:'10mb'}));
-app.use(express.json({limit:'10mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true, parameterLimit: 1000000}));
+app.use(bodyParser.json({limit: '50mb'}));
 app.use(userRoutes);
 
 app.listen(port,(err)=>{
